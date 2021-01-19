fece759a70bb691508b10b75f406a9bb96594855
TDK <khangtdk0612@gmail.com>
2021-01-19 15:07:37 +0700

    first commit

---
+++ src/component/Content/CardList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+
+export default function CardList() {
+    return (
+        <div className="row">
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
---
+++ src/component/Content/Container.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Info from './Info'
+import CardList from './CardList'
+
+export default function Container() {
+    return (
+        <div className="container">
+            <Info />
+            <CardList />
+        </div>
+    )
+}
